d4959892a2816e006625661dbf192765ae5c30d5
Disa Mhembere <dmhembe1@jhu.edu>
2015-11-22 16:20:23 -0500

    Setup modified for multiple files and numpy

--- MR-OCP/mrcap/utils/setup.py
+++ MR-OCP/mrcap/utils/setup.py
@@ -1,6 +1,25 @@
-from distutils.core import setup
+from distutils.core import setup, Extension
 from Cython.Build import cythonize
+from Cython.Distutils import build_ext
+
+import numpy
+
+"""
+setup(
+  ext_modules = cythonize("*.pyx")
+    )
+"""
+
+ext_modules=[
+      Extension("downsample", ["cy_downsample.pyx"], include_dirs=[numpy.get_include()]),
+      Extension("downsample_atlas", ["cy_downsample_atlas.pyx"]),
+]
+
+for e in ext_modules:
+    e.pyrex_directives = {"boundscheck": False}
 
 setup(
-        ext_modules = cythonize("*.pyx")
-        )
+    name = 'Down Sampler',
+    cmdclass = {'build_ext': build_ext},
+        ext_modules = ext_modules,
+)
