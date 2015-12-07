7bf574eb8f6a3c52497092a01d45c6dc2f074792
Disa Mhembere <dmhembe1@jhu.edu>
2015-12-07 14:08:11 -0500

    Cython in mrcap/utils setup script fixed for correct global installation

--- MR-OCP/mrcap/utils/setup.py
+++ MR-OCP/mrcap/utils/setup.py
@@ -4,22 +4,24 @@ from Cython.Distutils import build_ext
 
 import numpy
 
+"""
 setup(
+  name = "Downsample utilities",
+  cmdclass = {"build_ext": build_ext},
   ext_modules = cythonize("*.pyx")
     )
-
 """
+
 ext_modules=[
-      Extension("cy_downsample", ["cy_downsample.pyx"], include_dirs=[numpy.get_include()]),
-      Extension("cy_downsample_atlas", ["cy_downsample_atlas.pyx"]),
+      Extension("downsample", ["downsample.pyx"]),
+      Extension("downsample_atlas", ["downsample_atlas.pyx"]),
 ]
 
 for e in ext_modules:
     e.pyrex_directives = {"boundscheck": False}
 
 setup(
-    name = 'Down Sampler',
+    name = 'Downsample utilities',
     cmdclass = {'build_ext': build_ext},
         ext_modules = ext_modules,
 )
-"""
